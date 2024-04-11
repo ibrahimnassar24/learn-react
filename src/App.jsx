a53c1ade4294c2852784ac5ba21c5bc7abeb38ff
ibrahimnassar24 <74038100+ibrahimnassar24@users.noreply.github.com>
2024-04-11 10:49:05 +0200

    some template modifications

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+
+export default function App() {
+
+    return (
+        <>
+            <h1>hello to the app</h1>
+        </>
+    );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,57 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Root, {
-  loader as rootLoader,
-  action as rootAction
-} from './routes/root.jsx';
-import routes from "./routes/routeMap.jsx"
-import ErrorPage from './ErrorPage.jsx';
-import Contact, {
-  loader as contactLoader
-} from './routes/Contact.jsx';
-import EditContact, {
-  action as editContactAction
-} from './routes/EditContact.jsx';
-import {action as deleteAction} from "./routes/deleteContact.jsx"
-import Index from './routes/Index.jsx';
-import {action as contactAction} from "./routes/Contact.jsx"
+import React from "react";
+import ReactDom from "react-dom";
+import App from "./App";
+import "./index.css";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    errorElement: <ErrorPage />,
-    loader: rootLoader,
-    action: rootAction,
-    children: [
-      {
-        index: true,
-        element: <Index />
-      },
-      {
-        path: "/contacts/:contactId",
-        element: <Contact />,
-        loader: contactLoader,
-        action: contactAction
-      },
-      {
-        path: "/contacts/:contactId/edit",
-        element: <EditContact />,
-        loader: contactLoader,
-        action:editContactAction
-      },
-      {
-        path: "/contacts/:contactId/destroy",
-        action: deleteAction
-      }
-    ]
-  },
-]);
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
+ReactDom.createRoot(document.getElementById("root"))
+.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 )
